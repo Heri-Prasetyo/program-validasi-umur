307ef9af71fc5482e19b69a679ab4830fe6a1d4d
Heri Prasetyo <45659342+Heri-Prasetyo@users.noreply.github.com>
2018-12-17 14:13:36 +0700

    Add files via upload

---
+++ PROGRAM C++ VALIDASI UMUR 1 SAMPAI 100.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int umur;
+
+cout<<"PROGRAM VALIDASI UMUR"<<endl;
+
+cout<<"Input Tahun Kelahiran Anda = ";
+cin>>umur;
+umur=2018-umur;
+cout<<"Umur Anda Adalah           = "<<umur;
+return 0;
+
+
+
+
+}
